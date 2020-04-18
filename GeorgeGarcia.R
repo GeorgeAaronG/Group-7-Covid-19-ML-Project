4a324245d9cc8a0c82f74816e7bee2834f4a193e
GeorgeAaronG <george.aaron.garcia@gmail.com>
2020-04-18 18:26:03 -0500

    Successfully forecasted and plotted AAPL stock time series and market crash actuals.

--- GeorgeGarcia.R
+++ GeorgeGarcia.R
@@ -30,9 +30,26 @@ appleTS <- ts(appleTrain$price.close,
             end = c(2020, 02),
             freq = 97)
 
+appleValidTS <- ts(appleValid$price.close,
+                   start = c(2020, 02),
+                   end = c(2020, 04),
+                   freq = 41)
+
 appleAR <- tslm(appleTS ~ trend + I(trend^2))
 summary(appleAR)
 
 plot(appleTS, xlab = "Time", 
      ylab = "APPL Close Price",
      bty = "l")
+
+# Forecast after Feb 20, 2020
+applePredict <- forecast(appleAR, h = 41, level = 95)
+
+plot(applePredict, xlab = "Time",
+     ylab = "AAPL Close Price",
+     ylim = c(200, 400),
+     bty = "l",
+     xaxt = "n",
+     flty = 2)
+lines(applePredict$fitted, lwd = 2, col = "blue")
+lines(appleValidTS, lwd = 2, col = "red", lty = 3)
